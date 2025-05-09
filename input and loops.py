b705aa723e9b5ce1322e3b5e0ee0656e998eb3fe
Fir2f1y <theophilia.tay@kingsway.school.nz>
2025-05-09 14:11:01 +1200

    Add files via upload
    Demonstrated how to get an input from a user and store it

--- input and loops.py
+++ input and loops.py
@@ -1,11 +1,15 @@
 # To learn how to use input and loops
 # Theophilia Tay
-# Date 07/05/2025
-# Version 1
+# Date 09/05/2025
+# Version 2
 
 # Ask the user a question and store their answer in a variable
 # Ask the user for their name and store it
 name = input ("Hi, what is your name? ") #Stores answer as a string
+
+# Test that input was stored correctly
+print(f"Nice to meet you, {name}\n") # The \n adds a line break
+# To comment code out, use ctrl + /
 
 # Ask the user for two numbers and then add them together.
 num1 = int(input("What is your first number please? "))
@@ -15,8 +19,9 @@ print(f"You entered your second number as {num2}")
 
 # Adding the two answers together
 sum = num1 + num2
-print(sum)
+print(f"The two numbers added together will result in {sum},")
 
-#Test that input was stored correctly
-print(name)
-# To comment code out, use crtl + /
+# Multiplying the two inputs together
+multiply = num1 * num2
+print(f"The two numbers multiplied will result in {multiply}")
+
